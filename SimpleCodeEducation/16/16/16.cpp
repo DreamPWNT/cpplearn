033a59dab4ba67c67ef4831da64a995c3e4a2515
unknown <dreampwnt@gmail.com>
2023-10-11 18:34:00 +0300

    Finish 16

---
+++ SimpleCodeEducation/16/16/16.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int i = 0;
+
+	cout << "First for()" << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "i = " << i << endl;
+	}
+}
